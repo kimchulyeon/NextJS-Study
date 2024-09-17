280194be8cf13898a36a424b2cb1104b97f78e25
chulyeon <guinness987@gmail.com>
2024-09-17 19:32:11 +0900

    09.17 프리페칭

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello NextJS</h1>
+      <h1>Hello NextJS123</h1>
+      <Link href={"/search"}>Move To Search</Link>
     </div>
   );
 }
